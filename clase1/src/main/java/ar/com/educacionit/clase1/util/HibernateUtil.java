b108b58c9351c4bbff3bf0c1a70c08e2cfb46fb4
Fernando Carmine <carmine.fernando@gmail.com>
2024-09-26 18:41:38 -0300

    Clase 1 Hibernate

---
+++ clase1/src/main/java/ar/com/educacionit/clase1/util/HibernateUtil.java
@@ -0,0 +1,33 @@
+package ar.com.educacionit.clase1.util;
+
+public class HibernateUtil {
+	
+	// private static SessionFactory sessionFactory;
+
+	// static {
+	// Configuration configuration = new Configuration();
+	// configuration.configure("hibernate.cfg.xml");
+	// sessionFactory = configuration.buildSessionFactory();
+	// }
+
+	// public static SessionFactory getSessionFactory() {
+	// return sessionFactory;
+	// }
+
+	// public static Session getSession() {
+	// return sessionFactory.openSession();
+	// }
+
+	// public static void closeSession(Session session) {
+	// if (session != null) {
+	// session.close();
+	// }
+	// }
+
+	// public static void closeSessionFactory() {
+	// if (sessionFactory != null) {
+	// sessionFactory.close();
+	// }
+	// }
+
+}
